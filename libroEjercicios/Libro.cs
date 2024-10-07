82f241be2bfe571cea33ea038d8c73f94f6f81c2
Alumnos <Alumnos@DESKTOP-QSH1H77>
2024-10-07 20:49:43 -0300

    Agregar archivos de proyecto.

---
+++ libroEjercicios/Libro.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libroEjercicios
+{
+    internal class Libro
+    {
+        private static int countID = 1;
+
+        public int ID { get; private set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int AnioPublicacion { get; set; }
+
+        public Libro() { }
+
+        public Libro(string titulo, string autor, int anioPublicacion)
+        {
+            ID = countID++;
+            Titulo = titulo;
+            Autor = autor;
+            AnioPublicacion = anioPublicacion;
+        }
+
+        public override string ToString()
+        {
+            return $"Titulo: {Titulo}, Autor: {Autor}, Año de Publicación: {AnioPublicacion}. (ID: {ID})";
+        }
+    }
+}
